7ad93b61af3d5c00171f775b3e376a966107a833
ToyAuthor <eye5002003@gmail.com>
2017-12-04 15:19:00 +0800

    補上範例，這樣才能測試編好的第三方函式庫能不能用

---
+++ examples/UnicodeTest/UnicodeTest.cpp
@@ -0,0 +1,31 @@
+
+#include <toy/Standard.hpp>
+
+static int main2()
+{
+	toy::Log(u8"ToyBox;玩具箱;おもちゃ箱;장난감 상자;खिलौनो का बक्सा\n");
+	toy::Log( L"ToyBox;玩具箱;おもちゃ箱;장난감 상자;खिलौनो का बक्सा\n");
+
+	std::string    utf8 = u8"おもちゃ箱;";
+	std::wstring   wide = toy::utf::UTF8ToWChar(utf8);
+
+	toy::Logger<<u8"ToyBox;玩具箱;"<<wide<<u8"장난감 상자;"<<L"खिलौनो का बक्सा"<<toy::NextLine;
+
+	return EXIT_SUCCESS;
+}
+
+int main()
+{
+	int result = EXIT_FAILURE;
+
+	try
+	{
+		result = main2();
+	}
+	catch(std::exception &e)
+	{
+		toy::Log(e);
+	}
+
+	return result;
+}
